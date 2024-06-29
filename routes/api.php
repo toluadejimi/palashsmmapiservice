633405110d818fe96b9ccc6f03d93ebe8e66bd2a
Jimmy <toluadejimi@gmail.com>
2024-06-29 06:28:31 +0100

    bbhj

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,7 @@ use App\Http\Controllers\ApiController;
 Route::any('e-fund',  [ApiController::class,'e_fund']);
 Route::any('e-check',  [ApiController::class,'e_check']);
 
+Route::any('verify',  [ApiController::class,'verify_username']);
+
+
 
